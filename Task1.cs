da656a5e24a78621456a856971dbafc1f71efb31
GetLOWx <98523118+GetLOWx@users.noreply.github.com>
2022-01-30 16:38:51 +0200

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Task1InternshipReiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Hours : ");
+            int hours = int.Parse(Console.ReadLine());
+            Console.Write("Input Minutes : ");
+            int minutes = int.Parse(Console.ReadLine());
+
+            //Each hour on the clock represents an angle of 30 degrees (360 divided by 12)
+            double HID = (hours * 30) + (minutes * 30.0 / 60);
+            //each minute on the clock will represent an angle of 6 degrees (360 divided by 60)
+            double MID = minutes * 6;
+
+            double diff = Math.Abs(HID - MID);
+
+            if (diff > 180)
+            {
+                diff = 360 - diff;
+            }
+
+            Console.WriteLine($"Angle is {diff} degrees");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task23-BT-Recursion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Node
+{
+    public int value;
+    public Node left, right;
+
+    public Node(int val)
+    {
+        value = val;
+        left = right = null;
+    }
+}
+
+public class BinaryTree
+{
+    Node root;
+
+    int maxDepth(Node root)
+    {
+        if (root == null)
+
+            return 0;
+
+        // Recursively finding deth of subtrees
+        int LeftDepth = maxDepth(root.left);
+        int RightDepth = maxDepth(root.right);
+
+        // Get larger depth and +1
+        if (LeftDepth > RightDepth)
+            return (LeftDepth + 1);
+        else
+            return (RightDepth + 1);
+    }
+
+    public static void Main()
+    {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.right.left = new Node(5);
+        tree.root.right.right = new Node(6);
+        tree.root.right.left.left = new Node(7);
+        tree.root.right.right.left = new Node(8);
+        tree.root.right.right.right = new Node(9);
+        tree.root.right.right.left.left = new Node(10);
+        Console.WriteLine("Depth is: " + tree.maxDepth(tree.root));
+    }
+}
